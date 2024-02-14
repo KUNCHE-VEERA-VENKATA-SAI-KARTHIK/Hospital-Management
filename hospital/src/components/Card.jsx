2d8d18a818187db281d8637cc9091a8fd26ad635
Lakshmi-Jahnavi-Bavisetti <bljptp111@gmail.com>
2024-02-14 23:16:29 +0530

    updated pharmacy

--- hospital/src/components/Card.jsx
+++ hospital/src/components/Card.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 
 export default function Card(props) {
+  
   return (
     <>
     < div className='flex justify-center'>
-    <div className=' flex-col justify-center align-center'>
-        {/* <div>
-            <img src={props.image}/>
-        </div> */}
+    <div className=' flex-col justify-center align-center text-blue-600 border-2 border-sky-500 bg-slate-100'>
         <div>
-            <p>{props.title}</p>
-            <p>{props.price}</p>
+            <img className='ml-14 mr-10'  src={props.image}/>
         </div>
         <div >
-            <button>Add to Cart</button>
-            <button>Buy Now</button>
+            <p className='text-center '>{props.title}</p>
+            <p className='text-center'>Price : {props.price}</p>
         </div>
+        <div  flex-wrap gap-1>
+				<button
+					type="button"
+					class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+				>
+					ADD TO CART
+				</button>
+				<button 					class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+>
+					
+						BUY NOW !!
+
+				</button>
+			</div>
     </div>
     </div>
     </>
--- hospital/src/components/Pharmacy.jsx
+++ hospital/src/components/Pharmacy.jsx
@@ -3,21 +3,58 @@ import himalaya from '../assets/himalaya.jpg'
 import Card from './Card.jsx'
 
 export default function Pharmacy() {
+    // const brr=[
+    //     {
+    //         title:"Himalaya",
+    //         price:250
+    //     }];
+    // const [arr,setArr]=useState(brr)
 
-    const arr=[
+    // setArr((prev)=>{
+    //     [...prev,...brr]
+    // })
+
+    // console.log(arr)
+    let objArr = [
         {
-            title:"Himalaya",
-            price:250,
-        }];
+            title: 'himalaya',
+            price: 120,
+            image: himalaya
+        },
+        {
+            title: 'wow',
+            price: 150,
+            image: himalaya
+        },
+        {
+            title: 'patanjali',
+            price: 200,
+            image: himalaya
+        },
+        {
+            title: 'mamaearth',
+            price: 200,
+            image: himalaya
+        }
+    ];
+     
+    
 
   return (
     <>
+    <div className=' grid grid-cols-3 gap-4 justify-content-between mt-20'>
         {
-           arr.map((element)=>{
-            <Card title={element.title} price={element.price} />
-           })
+            objArr.map((item) => {
+                console.log(item);
+                return(
+                    <>
+                <Card  image={item.image} title={item.title} price={item.price} />
+                </>
+                )
+            })
+           
         }
-
+    </div>
 
     </>
   )
