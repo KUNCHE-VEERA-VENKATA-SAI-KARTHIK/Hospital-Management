7ed1d67ccd5cbbcfffbf0162321bc0b892ff27f9
Lakshmi-Jahnavi-Bavisetti <bljptp111@gmail.com>
2024-02-18 23:09:23 +0530

    updated homepage

---
+++ hospital/src/components/Tiles.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import child from  '../assets/child.jpg'
+
+export default function Tiles() {
+  return (
+    <> 
+    <div>
+        <div>
+            <img src='child.png' />
+        </div>
+    </div>
+    
+    </>
+  )
+}
