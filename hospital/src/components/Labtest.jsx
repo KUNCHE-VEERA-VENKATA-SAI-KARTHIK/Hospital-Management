f3b2068721df0dd9d387ec6189bfefaa5e1a852e
Lakshmi-Jahnavi-Bavisetti <bljptp111@gmail.com>
2024-05-08 20:18:58 +0530

    updated labtests

--- hospital/src/components/Labtest.jsx
+++ hospital/src/components/Labtest.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import diabetes from "../assets/Diabetes.webp";
 import Blood from "../assets/Blood Studies (Anemia).webp";
+import labbanner from "../assets/labbanner.png"
 // import Tax from "../assets/Tax Saver.webp";
 // import diabetes from "../assets/Diabetes.webp";
 // import diabetes from "../assets/Diabetes.webp";
@@ -13,15 +14,118 @@ import Blood from "../assets/Blood Studies (Anemia).webp";
 export default function Labtest() {
   const obj = [];
   return (
-    <div className="flex justify-center mt-4 mb-5">
-      <div className="shadow-lg pt-10 pb-5 rounded">
-        <div className="flex  content-center  justify-center ">
-          <img src={diabetes} className="h-50 w-20" />
+    <>
+    <div className="flex justify-center">
+    <div className="pt-8">
+            <img src={labbanner}  alt="c2" />
+    </div>
+    </div>
+
+    <div className="flex justify-center space-x-4 mt-4 mb-5">
+      <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+        <div className="  content-center  justify-center ">
+          <img src={diabetes} className="h-2% w-14" />
         </div>
-        <div className="flex  content-center  justify-center ">
+        <div className="pl-3 content-center  justify-center ">
           <p>diabetes</p>
         </div>
       </div>
+      <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+        <div className="  content-center  justify-center ">
+          <img src={diabetes} className="h-2% w-14" />
+        </div>
+        <div className="pl-3 content-center  justify-center ">
+          <p>diabetes</p>
+        </div>
+      </div>
+      <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+        <div className="  content-center  justify-center ">
+          <img src={diabetes} className="h-2% w-14" />
+        </div>
+        <div className="pl-3 content-center  justify-center ">
+          <p>diabetes</p>
+        </div>
+      </div>
+      <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+        <div className="  content-center  justify-center ">
+          <img src={diabetes} className="h-2% w-14" />
+        </div>
+        <div className="pl-3 content-center  justify-center ">
+          <p>diabetes</p>
+        </div>
+      </div>
+      <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+        <div className="  content-center  justify-center ">
+          <img src={diabetes} className="h-2% w-14" />
+        </div>
+        <div className="pl-3 content-center  justify-center ">
+          <p>diabetes</p>
+        </div>
+      </div>
+      <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+        <div className="  content-center  justify-center ">
+          <img src={diabetes} className="h-2% w-14" />
+        </div>
+        <div className="pl-3 content-center  justify-center ">
+          <p>diabetes</p>
+        </div>
+      </div>
+
     </div>
+
+<div className="flex justify-center space-x-4 mt-4 mb-5">
+<div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+  <div className="  content-center  justify-center ">
+    <img src={diabetes} className="h-2% w-14" />
+  </div>
+  <div className="pl-3 content-center  justify-center ">
+    <p>diabetes</p>
+  </div>
+</div>
+<div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+  <div className="  content-center  justify-center ">
+    <img src={diabetes} className="h-2% w-14" />
+  </div>
+  <div className="pl-3 content-center  justify-center ">
+    <p>diabetes</p>
+  </div>
+</div>
+<div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+  <div className="  content-center  justify-center ">
+    <img src={diabetes} className="h-2% w-14" />
+  </div>
+  <div className="pl-3 content-center  justify-center ">
+    <p>diabetes</p>
+  </div>
+</div>
+<div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+  <div className="  content-center  justify-center ">
+    <img src={diabetes} className="h-2% w-14" />
+  </div>
+  <div className="pl-3 content-center  justify-center ">
+    <p>diabetes</p>
+  </div>
+</div>
+<div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+  <div className="  content-center  justify-center ">
+    <img src={diabetes} className="h-2% w-14" />
+  </div>
+  <div className="pl-3 content-center  justify-center ">
+    <p>diabetes</p>
+  </div>
+</div>
+<div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
+  <div className="  content-center  justify-center ">
+    <img src={diabetes} className="h-2% w-14" />
+  </div>
+  <div className="pl-3 content-center  justify-center ">
+    <p>diabetes</p>
+  </div>
+</div>
+
+</div>
+</>
+    
+
   );
 }
