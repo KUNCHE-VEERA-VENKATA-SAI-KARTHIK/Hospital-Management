23b3137fb0394d078c005798518568918002de4e
Lakshmi-Jahnavi-Bavisetti <bljptp111@gmail.com>
2024-05-09 13:32:06 +0530

    Labtest Updated

--- hospital/src/components/Labtest.jsx
+++ hospital/src/components/Labtest.jsx
@@ -2,32 +2,35 @@ import React from "react";
 import diabetes from "../assets/Diabetes.webp";
 import Blood from "../assets/Blood Studies (Anemia).webp";
 import labbanner from "../assets/labbanner.png"
-// import Tax from "../assets/Tax Saver.webp";
-// import diabetes from "../assets/Diabetes.webp";
-// import diabetes from "../assets/Diabetes.webp";
-// import diabetes from "../assets/Diabetes.webp";
-// import diabetes from "../assets/Diabetes.webp";
-// import diabetes from "../assets/Diabetes.webp";
-// import diabetes from "../assets/Diabetes.webp";
-// import diabetes from "../assets/Diabetes.webp";
+import HealthPackage from "../assets/HealthPackage.webp";
+import Thyroid from "../assets/Thyroid.webp";
+import VITAMIN from "../assets/VITAMIN.webp";
+import WOMENSHEALTH from "../assets/WOMENS HEALTH.webp";
+import Kidney from "../assets/Kidney.webp";
+import HORMONE from "../assets/HORMONE.webp";
+import HAIRFALL from "../assets/Hairfall.webp";
+import FBC from "../assets/Full Body Checkup.webp";
+import Liver from "../assets/Liver.webp";
+import Fever from "../assets/Fever.webp";
 
 export default function Labtest() {
   const obj = [];
   return (
     <>
-    <div className="flex justify-center">
+    
+    <div className="flex  justify-center">
     <div className="pt-8">
             <img src={labbanner}  alt="c2" />
     </div>
     </div>
-
+    <div  className="h-10">
     <div className="flex justify-center space-x-4 mt-4 mb-5">
       <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
         <div className="  content-center  justify-center ">
-          <img src={diabetes} className="h-2% w-14" />
+          <img src={FBC} className="h-2% w-14" />
         </div>
         <div className="pl-3 content-center  justify-center ">
-          <p>diabetes</p>
+          <p>Full Body Checkup</p>
         </div>
       </div>
       <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
@@ -40,34 +43,34 @@ export default function Labtest() {
       </div>
       <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
         <div className="  content-center  justify-center ">
-          <img src={diabetes} className="h-2% w-14" />
+          <img src={WOMENSHEALTH} className="h-2% w-14" />
         </div>
         <div className="pl-3 content-center  justify-center ">
-          <p>diabetes</p>
+          <p>Women's health</p>
         </div>
       </div>
       <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
         <div className="  content-center  justify-center ">
-          <img src={diabetes} className="h-2% w-14" />
+          <img src={Thyroid} className="h-2% w-14" />
         </div>
         <div className="pl-3 content-center  justify-center ">
-          <p>diabetes</p>
+          <p>Thyroid</p>
         </div>
       </div>
       <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
         <div className="  content-center  justify-center ">
-          <img src={diabetes} className="h-2% w-14" />
+          <img src={HealthPackage} className="h-2% w-14" />
         </div>
         <div className="pl-3 content-center  justify-center ">
-          <p>diabetes</p>
+          <p>Health Package</p>
         </div>
       </div>
       <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
         <div className="  content-center  justify-center ">
-          <img src={diabetes} className="h-2% w-14" />
+          <img src={Fever} className="h-2% w-14" />
         </div>
         <div className="pl-3 content-center  justify-center ">
-          <p>diabetes</p>
+          <p>Fever</p>
         </div>
       </div>
 
@@ -76,53 +79,54 @@ export default function Labtest() {
 <div className="flex justify-center space-x-4 mt-4 mb-5">
 <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
   <div className="  content-center  justify-center ">
-    <img src={diabetes} className="h-2% w-14" />
+    <img src={VITAMIN} className="h-2% w-14" />
   </div>
   <div className="pl-3 content-center  justify-center ">
-    <p>diabetes</p>
+    <p>Vitamin</p>
   </div>
 </div>
 <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
   <div className="  content-center  justify-center ">
-    <img src={diabetes} className="h-2% w-14" />
+    <img src={Blood} className="h-2% w-14" />
   </div>
   <div className="pl-3 content-center  justify-center ">
-    <p>diabetes</p>
+    <p>Blood Test</p>
   </div>
 </div>
 <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
   <div className="  content-center  justify-center ">
-    <img src={diabetes} className="h-2% w-14" />
+    <img src={Liver} className="h-2% w-14" />
   </div>
   <div className="pl-3 content-center  justify-center ">
-    <p>diabetes</p>
+    <p>Liver</p>
   </div>
 </div>
 <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
   <div className="  content-center  justify-center ">
-    <img src={diabetes} className="h-2% w-14" />
+    <img src={HAIRFALL} className="h-2% w-14" />
   </div>
   <div className="pl-3 content-center  justify-center ">
-    <p>diabetes</p>
+    <p>Hairfall</p>
   </div>
 </div>
 <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
   <div className="  content-center  justify-center ">
-    <img src={diabetes} className="h-2% w-14" />
+    <img src={Kidney} className="h-2% w-14" />
   </div>
   <div className="pl-3 content-center  justify-center ">
-    <p>diabetes</p>
+    <p>Kidney</p>
   </div>
 </div>
 <div className=" flex border pt-2 pb-2 pl-10 pr-10 rounded">
   <div className="  content-center  justify-center ">
-    <img src={diabetes} className="h-2% w-14" />
+    <img src={HORMONE} className="h-2% w-14" />
   </div>
   <div className="pl-3 content-center  justify-center ">
-    <p>diabetes</p>
+    <p>Harmone</p>
   </div>
 </div>
 
+</div>
 </div>
 </>
     
