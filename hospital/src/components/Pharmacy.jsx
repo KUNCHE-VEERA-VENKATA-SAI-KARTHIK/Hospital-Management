02b3680a5e3d148aa3c8ac527aded8d95bc6c832
Lakshmi-Jahnavi-Bavisetti <bljptp111@gmail.com>
2024-05-17 12:04:01 +0530

    updated pharmacy

--- hospital/src/components/Pharmacy.jsx
+++ hospital/src/components/Pharmacy.jsx
@@ -22,21 +22,38 @@ export default function Pharmacy() {
     
 
 return(
-  <div className='flex-'>
-  Pharmacy
-
+  <>
+  <div className='flex justify-center'>
+    <h1 className='text-2xl font-bold'>pharmacy</h1>
+  </div>
+  <div className=' grid grid-cols-4 gap-20 justify-between border-none ml-5 mr-5 mt-6	'>
   {data ? (
     data.map((detail) => (
       <div key={detail.id}>
+        <div className=' flex justify-center w-26 h-40'>
         <img src={detail.image} alt={detail.title} />
-        <p>{detail.title}</p>
+        </div>
+        <div className=' flex justify-center'>
+        <h2>{detail.title}</h2>
+        </div>
+        <div className=' flex justify-center'>
         <p>{detail.price}</p>
+        </div>
+        <div className=" flex justify-between flex-row ">
+          <button className='border ml-5 pl-4 pr-4 pt-2 pb-2'  >
+            buy now 
+          </button>
+          <button  className='border mr-5 '  >
+          add to cart
+          </button>
+        </div>
       </div>
     ))
   ) : (
     <p>Loading data...</p>
   )}
 </div>
+</>
 )
    
 }
