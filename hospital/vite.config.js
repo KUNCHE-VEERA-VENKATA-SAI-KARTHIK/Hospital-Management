3170fcae78a4d83052120ed394fce8900558ed23
kunchesaikarthik <kunchesaikarthik999@gmail.com>
2024-07-26 02:39:08 +0530

    change3

--- hospital/vite.config.js
+++ hospital/vite.config.js
@@ -1,6 +1,7 @@
-// vite.config.js
-export default {
-  build: {
-    outDir: 'dist',
-  },
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
